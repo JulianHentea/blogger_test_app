523c67ac180340871173a5102826913e75650afc
Julian Hentea <j.g.hentea@gmail.com>
2013-05-22 06:19:30 -0600

    forn-based workflow featured compleated

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,9 +1,46 @@
 class ArticlesController < ApplicationController
+  
   def index
     @articles = Article.all
   end
+  
   def show
     @article = Article.find(params[:id]) 
   end
   
+  def new 
+   @article = Article.new 
+  end
+  
+  def create
+    @article = Article.new(params[:article])
+    @article.save
+    
+    flash.notice = "Article '#{@article.title}' Saved!"
+    
+    
+    redirect_to article_path(@article)
+  end
+  
+  def destroy
+    @article = Article.destroy (params[:id])
+    
+    flash.notice = "Article '#{@article.title}' Deleted!"
+    
+    
+    redirect_to articles_url
+  end
+  
+  def edit
+   @article = Article.find(params[:id])
+  end
+  
+  def update 
+    @article = Article.find(params[:id])
+    @article.update_attributes(params[:article])
+    
+    flash.notice = "Article '#{@article.title}' Updated!"
+  
+    redirect_to article_path (@article)
+  end
 end
